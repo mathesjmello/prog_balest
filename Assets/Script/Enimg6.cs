5e55f53709e5dec569df86bd896588a3eb5fef64
matheus <junqueirademello@gmail.com>
2019-05-28 23:38:44 -0300

    prefab dos inimg 5 e 6

---
+++ Assets/Script/Enimg6.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Script
+{
+    public class Enimg6: BaseEnimig
+    {
+        
+        void Update()
+        {
+            Move();
+        }
+
+        public override void Move()
+        {
+            float step = Speed * Time. deltaTime;
+            transform. position = Vector3. MoveTowards(transform. position, new Vector3(TarguetTransform.position.x+Random.Range(-10,10), TarguetTransform.position.y+3, TarguetTransform.position.z+Random.Range(-1,1)), step);
+        }
+    }
+}
--- Assets/Script/SpamManeger.cs
+++ Assets/Script/SpamManeger.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Script
 {
     public class SpamManeger : MonoBehaviour
     {
-        public BaseEnimig Enimg1,Enimg2, Enimg3, Enimg4;
+        public BaseEnimig Enimg1,Enimg2, Enimg3, Enimg4, Enimg5, Enimg6;
         public Transform SpamTrans1;
         private Vector3 _spamPosi1;
         public float CriateTime=12;
@@ -17,9 +18,12 @@ namespace Script
             Invoke(nameof(Criate2), CriateTime*1.4f);
             Invoke(nameof(Criate3), CriateTime*2f);
             Invoke(nameof(Criate4), CriateTime*2.3f);
+            Invoke(nameof(Criate5), CriateTime * 1.7f);
+            Invoke(nameof(Criate6), CriateTime * 3f);
         }
 
-    
+        
+
         void Criate1()
         {
             Enimg1.Create(new Vector3(Random.Range(-20, 20), _spamPosi1.y, _spamPosi1.z), SpamTrans1.rotation);
@@ -39,6 +43,16 @@ namespace Script
         {
             Enimg4.Create(new Vector3(Random.Range(-20, 20), _spamPosi1.y, _spamPosi1.z), SpamTrans1.rotation);
             Invoke(nameof(Criate4), CriateTime*2.3f);
+        }
+        void Criate5()
+        {
+            Enimg5.Create(new Vector3(Random.Range(-20, 20), _spamPosi1.y+3, _spamPosi1.z), SpamTrans1.rotation);
+            Invoke(nameof(Criate5), CriateTime * 1.7f);
+        }
+        void Criate6()
+        {
+            Enimg6.Create(new Vector3(Random.Range(-20, 20), _spamPosi1.y+3, _spamPosi1.z), SpamTrans1.rotation);
+            Invoke(nameof(Criate6), CriateTime * 3f);
         }
     }
 }
